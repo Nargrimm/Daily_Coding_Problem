c10d8b47ba4e8cdad4677fa304340b0546a4855c
Victor LE CORNEC <nargrimm@gmail.com>
2019-11-04 22:14:27 +0100

    Day138: Add day138 solution in cpp

---
+++ day138/day138.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> get_min_coins(int value)
+{
+    std::vector<int> res = {};
+    std::vector<int> coins = {25, 10, 5, 1};
+    while (value > 0)
+    {
+        for (auto const coin : coins)
+        {
+            if (value >= coin)
+            {
+                value -= coin;
+                res.push_back(coin);
+                break;
+            }
+        }
+    }
+    return res;
+}
+
+void print_results(std::vector<int> res)
+{
+    if (!res.size() > 0)
+        return;
+    std::cout << "[" << *(res.begin());
+    for (auto it = res.begin() + 1; it != res.end(); it++)
+    {
+        std::cout << ", " << *it;
+    }
+    std::cout << "]" << std::endl;
+    return;
+}
+
+int main(void)
+{
+    print_results(get_min_coins(16));
+    print_results(get_min_coins(37));
+    print_results(get_min_coins(0));
+    print_results(get_min_coins(-1));
+    print_results(get_min_coins(128));
+    return 0;
+}
